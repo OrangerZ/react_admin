1e589c059ba51f3cef19d74e91160c8bcf8de75a
OrangerZ <281998151@qq.com>
2021-05-24 17:26:46 -0400

    new web

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import React,{Component} from 'react';
+import { message, Button, Space } from 'antd';
+/*root component*/
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+export default class App extends Component {
+
+    success =()=>{
+        message.success ('This is a success message');
+    };
+
+    render() {
+        return <Button type="dashed" onClick={this.success}>Dashed Button</Button>
+    };
 }
-
-export default App;
